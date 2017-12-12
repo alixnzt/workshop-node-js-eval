6eb2c98f4dbb544b0182b0e0a246b5b8b6e8370c
alix nzt <alix.nouzillat@esophro.fr>
2017-12-12 16:59:25 +0100

    Exercise 3

--- src/exercise3.js
+++ src/exercise3.js
@@ -1,3 +1,30 @@
+var input = [
+  { operator: 'add', value: 2 },
+  { operator: 'substract', value: 4 },
+  { operator: 'add', value: 3 }
+]
+
+var result = 0
+
+//console.log(input);
+for (var i = 0; i <= input.length-1; i++) {
+	var operation = input[i]
+  	console.log("-----------")
+ 	//console.log("operation :", operation);
+	// console.log("operator : ", operation.operator)
+	// console.log("value : ", operation.value)
+
+	if (operation.operator === 'add') {
+  		return result = result + operation.value;
+	  }
+	  else if (operation.operator === 'substract') {
+	  	return result = result - operation.value;
+	  }
+  }
+  console.log(result);
+
+
 exports.run = function(input) {
   console.log("Exercise #3");
+  
 };
