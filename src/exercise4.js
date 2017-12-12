0541742566a9d83481bb0eaca1237ac21f4bb116
alix nzt <alix.nouzillat@esophro.fr>
2017-12-12 16:59:58 +0100

    Exercise 4 pas de modif

--- src/exercise4.js
+++ src/exercise4.js
@@ -2,6 +2,8 @@ exports.run = function(input) {
   console.log("Exercise #4");
 
   // YOUR CODE IS WELCOME HERE
+  
+  //console.log(input)  
 
   /** STRICTLY FORBIDDEN TO CHANGE THIS CODE */
   return input(myFunction, [1, 2, 3], ["One", "Two", "Three"]);
