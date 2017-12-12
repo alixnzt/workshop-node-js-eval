d8acb64dfb66bc49a2f528a66d4b1c8874926fd8
alix nzt <alix.nouzillat@esophro.fr>
2017-12-12 17:00:39 +0100

    Exercise 5

--- src/exercise5.js
+++ src/exercise5.js
@@ -1,3 +1,10 @@
+var fs = require('fs');
+var file = process.argv[2];
+var file = fs.readFile(file, (err, data) => {
+	//var output = data;
+	console.log(data)
+})
+
 exports.run = function(input) {
   console.log("Exercise #5");
 };
